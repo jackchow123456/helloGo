4e8ae38daf20670d4404cd28c738c29f177dfda3
周民杰 <zhouminjie@ydjdev.com>
2021-12-08 00:33:18 +0800

    反射

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"reflect"
 	"strings"
 	"unsafe"
 )
@@ -656,7 +657,7 @@ func main() {
 	// 接收者和指针接收者实现接口的区别
 	// 不管是dog结构体还是结构体指针*dog类型的变量都可以赋值给该接口变量。因为Go语言中有对指针类型变量求值的语法糖，dog指针fugui内部会自动求值*fugui。
 
-	// 面试题 - 首先请观察下面的这段代码，然后请回答这段代码能不能通过编译？ 
+	// 面试题 - 首先请观察下面的这段代码，然后请回答这段代码能不能通过编译？
 	// 不能，
 	// var peo Peoplet = Studentt{}
 	// think := "bitch"
@@ -686,7 +687,7 @@ func main() {
 	// x = h
 	// x.wash()
 	// x.dry()
-	
+
 	// 接口嵌套
 	// 接口与接口间可以通过嵌套创造出新的接口。
 	// 嵌套得到的接口的使用与普通接口一样，这里我们让cat实现animal接口：
@@ -720,15 +721,168 @@ func main() {
 	// fmt.Println(studentInfo)
 
 	// 类型断言
-	xx = "Hello 沙河"
-	vv, ok := xx.(string)
-	if ok {
-		fmt.Println(vv)
-	} else {
-		fmt.Println("类型断言失败")
-	}
+	// xx = "Hello 沙河"
+	// vv, ok := xx.(string)
+	// if ok {
+	// 	fmt.Println(vv)
+	// } else {
+	// 	fmt.Println("类型断言失败")
+	// }
 
 	/** Go语言基础之反射 **/
+	// reflect包，，任何接口值都由是一个具体类型和具体类型的值两部分组成的(我们在上一篇接口的博客中有介绍相关概念)。
+	// 在Go语言中反射的相关功能由内置的reflect包提供，任意接口值在反射中都可以理解为由reflect.Type和reflect.Value两部分组成，
+	// 并且reflect包提供了reflect.TypeOf和reflect.ValueOf两个函数来获取任意对象的Value和Type。
+	// 在Go语言中，使用reflect.TypeOf()函数可以获得任意值的类型对象（reflect.Type），程序通过类型对象可以访问任意值的类型信息
+	// var aa int32 = 100
+	// reflectType(aa)
+
+	// type name和type kind
+	// type myInt int64
+	// var a *float32 // 指针
+	// var b myInt    // 自定义类型
+	// var c rune     // 类型别名
+	// reflectType(a) // type: kind:ptr
+	// reflectType(b) // type:myInt kind:int64
+	// reflectType(c) // type:int32 kind:int32
+
+	// var d = person{
+	// 	name: "沙河小王子",
+	// 	age:  18,
+	// }
+	// var e = book{title: "《跟小王子学Go语言》"}
+	// reflectType(d) // type:person kind:struct
+	// reflectType(e) // type:book kind:struct
+
+	// ValueOf
+	// reflect.ValueOf()返回的是reflect.Value类型，其中包含了原始值的值信息。reflect.Value与原始值之间可以互相转换。
+
+	// var a float32 = 3.14
+	// var b int64 = 100
+	// reflectValue(a) // type is float32, value is 3.140000
+	// reflectValue(b) // type is int64, value is 100
+	// // 将int类型的原始值转换为reflect.Value类型
+	// c := reflect.ValueOf(10)
+	// fmt.Printf("type c :%T\n", c) // type c :reflect.Value
+
+	// 通过反射设置变量的值
+	// 想要在函数中通过反射修改变量的值，需要注意函数参数传递的是值拷贝，必须传递变量地址才能修改变量值。而反射中使用专有的Elem()方法来获取指针对应的值。
+	// var a int64 = 100
+	// // reflectSetValue1(a) //panic: reflect: reflect.Value.SetInt using unaddressable value
+	// reflectSetValue2(&a)
+	// fmt.Println(a)
+
+	// isNil()和isValid()
+	// IsNil()报告v持有的值是否为nil。v持有的值的分类必须是通道、函数、接口、映射、指针、切片之一；否则IsNil函数会导致panic。
+	// IsValid()返回v是否持有一个值。如果v是Value零值会返回假，此时v除了IsValid、String、Kind之外的方法都会导致panic。
+
+	// // *int类型空指针
+	// var a *int
+	// fmt.Println("var a *int IsNil:", reflect.ValueOf(a).IsNil())
+	// // nil值
+	// fmt.Println("nil IsValid:", reflect.ValueOf(nil).IsValid())
+	// // 实例化一个匿名结构体
+	// b := struct{}{}
+	// // 尝试从结构体中查找"abc"字段
+	// fmt.Println("不存在的结构体成员:", reflect.ValueOf(b).FieldByName("abc").IsValid())
+	// // 尝试从结构体中查找"abc"方法
+	// fmt.Println("不存在的结构体方法:", reflect.ValueOf(b).MethodByName("abc").IsValid())
+	// // map
+	// c := map[string]int{}
+	// // 尝试从map中查找一个不存在的键
+	// fmt.Println("map中不存在的键：", reflect.ValueOf(c).MapIndex(reflect.ValueOf("娜扎")).IsValid())
+
+	// 结构体反射
+
+	stu1 := student{
+		Name:  "小王子",
+		Score: 90,
+	}
+
+	// t := reflect.TypeOf(stu1)
+	// fmt.Println(t.Name(), t.Kind()) // student struct
+	// // 通过for循环遍历结构体的所有字段信息
+	// for i := 0; i < t.NumField(); i++ {
+	// 	field := t.Field(i)
+	// 	fmt.Printf("name:%s index:%d type:%v json tag:%v\n", field.Name, field.Index, field.Type, field.Tag.Get("json"))
+	// }
+
+	// // 通过字段名获取指定结构体字段信息
+	// if scoreField, ok := t.FieldByName("Score"); ok {
+	// 	fmt.Printf("name:%s index:%d type:%v json tag:%v\n", scoreField.Name, scoreField.Index, scoreField.Type, scoreField.Tag.Get("json"))
+	// }
+
+	printMethod(stu1)
+}
+func printMethod(x interface{}) {
+	t := reflect.TypeOf(x)
+	v := reflect.ValueOf(x)
+
+	fmt.Println(t.NumMethod())
+	for i := 0; i < v.NumMethod(); i++ {
+		methodType := v.Method(i).Type()
+		fmt.Printf("method name:%s\n", t.Method(i).Name)
+		fmt.Printf("method:%s\n", methodType)
+		// 通过反射调用方法传递的参数必须是 []reflect.Value 类型
+		var args = []reflect.Value{}
+		v.Method(i).Call(args)
+	}
+}
+
+// 给student添加两个方法 Study和Sleep(注意首字母大写)
+func (s student) Study() string {
+	msg := "好好学习，天天向上。"
+	fmt.Println(msg)
+	return msg
+}
+
+func (s student) Sleep() string {
+	msg := "好好睡觉，快快长大。"
+	fmt.Println(msg)
+	return msg
+}
+
+type student struct {
+	Name  string `json:"name"`
+	Score int    `json:"score"`
+}
+
+func reflectSetValue1(x interface{}) {
+	v := reflect.ValueOf(x)
+	if v.Kind() == reflect.Int64 {
+		v.SetInt(200) //修改的是副本，reflect包会引发panic
+	}
+}
+func reflectSetValue2(x interface{}) {
+	v := reflect.ValueOf(x)
+	// 反射中使用 Elem()方法获取指针对应的值
+	if v.Elem().Kind() == reflect.Int64 {
+		v.Elem().SetInt(200)
+	}
+}
+
+func reflectValue(x interface{}) {
+	v := reflect.ValueOf(x)
+	k := v.Kind()
+	switch k {
+	case reflect.Int64:
+		// v.Int()从反射中获取整型的原始值，然后通过int64()强制类型转换
+		fmt.Printf("type is int64, value is %d\n", int64(v.Int()))
+	case reflect.Float32:
+		// v.Float()从反射中获取浮点型的原始值，然后通过float32()强制类型转换
+		fmt.Printf("type is float32, value is %f\n", float32(v.Float()))
+	case reflect.Float64:
+		// v.Float()从反射中获取浮点型的原始值，然后通过float64()强制类型转换
+		fmt.Printf("type is float64, value is %f\n", float64(v.Float()))
+	}
+}
+
+type book struct{ title string }
+
+func reflectType(x interface{}) {
+	v := reflect.TypeOf(x)
+	// fmt.Printf("type:%v\n", v)
+	fmt.Printf("type:%v kind:%v\n", v.Name(), v.Kind())
 }
 
 var xx interface{}
@@ -738,61 +892,61 @@ func show(a interface{}) {
 	fmt.Printf("type:%T value:%v\n", a, a)
 }
 
-type animal interface{
+type animal interface {
 	Mover
 	Sayer
 }
 
-type WashingMachine interface{
+type WashingMachine interface {
 	wash()
 	dry()
 }
 
-type Dryer struct {}
+type Dryer struct{}
 
-func (d Dryer) dry(){
+func (d Dryer) dry() {
 	fmt.Println("脱水烘干")
 }
 
-type haier struct{
+type haier struct {
 	Dryer
 }
 
-func (h haier) wash(){
+func (h haier) wash() {
 	fmt.Println("洗衣服")
 }
 
-type catt struct{
+type catt struct {
 	branch string
 }
 
-type dogg struct{
-	name string 
+type dogg struct {
+	name string
 }
 
-func (d dogg) say(){
-	fmt.Printf("%s叫了：汪汪汪\n",d.name)
+func (d dogg) say() {
+	fmt.Printf("%s叫了：汪汪汪\n", d.name)
 }
 
-func (d dogg) move(){
-	fmt.Printf("%s走了\n",d.name)
+func (d dogg) move() {
+	fmt.Printf("%s走了\n", d.name)
 }
 
-func (c catt) move(){
-	fmt.Printf("%s速度70迈\n",c.branch)
+func (c catt) move() {
+	fmt.Printf("%s速度70迈\n", c.branch)
 }
 
-func (c catt) say(){
-	fmt.Printf("%s叫了：猫猫猫\n",c.branch)
+func (c catt) say() {
+	fmt.Printf("%s叫了：猫猫猫\n", c.branch)
 }
 
 // Sayer 接口
-type Sayer interface{
+type Sayer interface {
 	say()
 }
 
 // Mover 接口
-type Mover interface{
+type Mover interface {
 	move()
 }
 
