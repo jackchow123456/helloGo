9e5d664a48c003865c68d9b56b60545e909b3e7e
周民杰 <zhouminjie@ydjdev.com>
2021-12-08 23:42:59 +0800

    goroutine

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"math"
 	"reflect"
 	"strings"
+	"sync"
+	"time"
 	"unsafe"
 )
 
@@ -794,10 +796,10 @@ func main() {
 
 	// 结构体反射
 
-	stu1 := student{
-		Name:  "小王子",
-		Score: 90,
-	}
+	// stu1 := student{
+	// 	Name:  "小王子",
+	// 	Score: 90,
+	// }
 
 	// t := reflect.TypeOf(stu1)
 	// fmt.Println(t.Name(), t.Kind()) // student struct
@@ -812,8 +814,106 @@ func main() {
 	// 	fmt.Printf("name:%s index:%d type:%v json tag:%v\n", scoreField.Name, scoreField.Index, scoreField.Type, scoreField.Tag.Get("json"))
 	// }
 
-	printMethod(stu1)
+	// printMethod(stu1)
+
+	/** Go语言中的并发编程 **/
+	// 	goroutine
+	// go hello()
+	// fmt.Println("main goroutine done!")
+	// time.Sleep(time.Second)
+
+	// 启动多个goroutine
+	// for i := 0; i < 10; i++ {
+	// 	wg.Add(1) // 启动一个goroutine就登记+1
+	// 	go hello(i)
+	// }
+	// wg.Wait() // 等待所有登记的goroutine都结束
+
+	// goroutine与线程
+
+	// channel
+	// ch := make(chan int, 1) // 创建一个容量为1的有缓冲区通道
+	// ch <- 10
+	// fmt.Println("发送成功")
+
+	// ch1 := make(chan int)
+	// ch2 := make(chan int)
+	// // 开启goroutine将0~100的数发送到ch1中
+	// go func() {
+	// 	for i := 0; i < 100; i++ {
+	// 		ch1 <- i
+	// 	}
+	// 	close(ch1)
+	// }()
+	// // 开启goroutine从ch1中接收值，并将该值的平方发送到ch2中
+	// go func() {
+	// 	for {
+	// 		i, ok := <-ch1 // 通道关闭后再取值ok=false
+	// 		if !ok {
+	// 			break
+	// 		}
+	// 		ch2 <- i * i
+	// 	}
+	// 	close(ch2)
+	// }()
+	// // 在主goroutine中从ch2中接收值打印
+	// for i := range ch2 { // 通道关闭后会退出for range循环
+	// 	fmt.Println(i)
+	// }
+
+	// worker pool（goroutine池）
+	// 在工作中我们通常会使用可以指定启动的goroutine数量–worker pool模式，控制goroutine的数量，防止goroutine泄漏和暴涨。
+	// 一个简易的work pool示例代码如下：
+
+	// jobs := make(chan int, 100)
+	// results := make(chan int, 100)
+	// // 开启3个goroutine
+	// for w := 1; w <= 3; w++ {
+	// 	go worker(w, jobs, results)
+	// }
+	// // 5个任务
+	// for j := 1; j <= 5; j++ {
+	// 	jobs <- j
+	// }
+	// close(jobs)
+	// // 输出结果
+	// for a := 1; a <= 5; a++ {
+	// 	<-results
+	// }
+
+	// select多路复用
+	// 在某些场景下我们需要同时从多个通道接收数据。通道在接收数据时，如果没有数据可以接收将会发生阻塞。你也许会写出如下代码使用遍历的方式来实现：
+
+	ch := make(chan int, 1)
+	for i := 0; i < 10; i++ {
+		select {
+		case x := <-ch:
+			fmt.Println(x)
+		case ch <- i:
+		}
+	}
 }
+
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for j := range jobs {
+		fmt.Printf("worker:%d start job:%d\n", id, j)
+		time.Sleep(time.Second)
+		fmt.Printf("worker:%d end job:%d\n", id, j)
+		results <- j * 2
+	}
+}
+
+var wg sync.WaitGroup
+
+func hello(i int) {
+	defer wg.Done() // goroutine结束就登记-1
+	fmt.Println("Hello Goroutine!", i)
+}
+
+// func hello() {
+// 	fmt.Println("Hello Goroutine!")
+// }
+
 func printMethod(x interface{}) {
 	t := reflect.TypeOf(x)
 	v := reflect.ValueOf(x)
